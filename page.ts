629f9dc87373937539df5836b7c083b00b8d54e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-05 08:22:00 +0000

    Update page.ts

--- page.ts
+++ page.ts
@@ -1,14 +1,14 @@
 import MarkdownIt from "https://esm.sh/markdown-it@13"
-import mathjax from "https://esm.sh/markdown-it-mathjax3@4"
-import anchor from "https://esm.sh/markdown-it-anchor@8"
-import footnote from "https://esm.sh/markdown-it-footnote@3"
-import highlightjs from "https://esm.sh/markdown-it-highlightjs@4"
-import wikilinks from "https://esm.sh/markdown-it-wikilinks@1"
+import mathjax from "https://esm.sh/markdown-it-mathjax3@4?bundle"
+import anchor from "https://esm.sh/markdown-it-anchor@8?bundle"
+import footnote from "https://esm.sh/markdown-it-footnote@3?bundle"
+import highlightjs from "https://esm.sh/markdown-it-highlightjs@4?bundle"
+import wikilinks from "https://esm.sh/markdown-it-wikilinks@1?bundle"
 
 import * as MediaType from "https://deno.land/x/media_types@v2.3.7/mod.ts";
-import * as Eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 import * as Yaml  from "https://deno.land/std@0.114.0/encoding/yaml.ts";
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { compile } from "https://pax.deno.dev/tani/deno-template"
 
 const markdownIt = MarkdownIt()
     .use(mathjax)
@@ -21,7 +21,10 @@ const { pathname: dirname } = new URL('.', import.meta.url)
 
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url)
-  const relativePathname = dirname + pathname.slice(1)
+  let relativePathname = dirname + pathname.slice(1)
+  if (relativePathname === dirname) {
+    relativePathname = dirname + 'index.html'
+  }
   try {
     const blob = await Deno.readFile(relativePathname)
     const contentType = MediaType.lookup(relativePathname) ?? 'text/plain'
@@ -29,17 +32,17 @@ async function handleRequest(request: Request): Promise<Response> {
     return new Response(blob, options)
   } catch {
     try {
-      const text = await Deno.readTextFile(relativePathname + '.md')
-      const yaml = text.replace(/----+/, '').replace(/----+[\s\S]*/, '')
+      const text = await Deno.readTextFile(relativePathname.replace(/\.html$/, '.md'))
+      const yaml = text.replace(/---+/, '').replace(/---+[\s\S]*/, '')
       const header = Yaml.parse(yaml) as Record<string, unknown> 
       try {
         const layout = await Deno.readTextFile(dirname + header.layout + '.ejs')
-        const markdown = text.replace(/----+[\s\S]+?----+/, '')
+        const markdown = text.replace(/---+[\s\S]+?---+/, '')
         const content = markdownIt.render(markdown) 
-        const html = await Eta.render(layout, { ...header, content })
+        const html = await compile(layout)({ ...header, content })
         const options = { headers: { 'content-type': 'text/html' } }
         return new Response(html as string, options )
-      } catch (err) {
+      } catch (_) {
         return new Response('', { status: 500 })
       }
     } catch {
