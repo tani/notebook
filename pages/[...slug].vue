cdcac3af8ed39cdfa3668ab24d79bab1081cb1f1
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-05 15:42:47 +0900

    Delete nuxt

--- pages/[...slug].vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <article>
-    <ContentDoc />
-  </article>
-</template>
--- server/plugins/content.ts
+++
@@ -1,7 +0,0 @@
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('content:file:beforeParse', (file) => {
-    if (file._id.endsWith('md')) {
-      file.body = file.body.replace(/```mermaid([\s\S]*?)```/g, '<mermaid>$1</mermaid>')
-    }
-  })
-})
