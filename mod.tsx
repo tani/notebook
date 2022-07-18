eca76a38683319a4e39b60211e6e8b69eda01ab9
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-18 13:32:58 +0900

    Update mod.tsx

--- mod.tsx
+++ mod.tsx
@@ -13,7 +13,7 @@ import frontmatter from "http://esm.sh/markdown-it-front-matter@0.2.3?bundle";
 import YAML from "https://esm.sh/yaml@2?bundle"
 
 import { serve } from "https://deno.land/std@0.147.0/http/server.ts";
-import { Hono } from "https://deno.land/x/hono@v2.0.0/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v2.0.2/mod.ts";
 import { jsx, serveStatic, compress } from 'https://deno.land/x/hono@v2.0.0/middleware.ts'
 
 
